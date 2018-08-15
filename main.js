6ebf108431bba139ba85d03c5636e2d65be583c1
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-08-15 10:46:05 -0600

    Got the convert to work with input

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-
+let rate;
 
 axios.get('https://api.exchangeratesapi.io/latest')
   .then(function (result) {
-    console.log(result)
+    console.log(result.data.rates['AUD'])
+    rate = result.data.rates['AUD']
     document.querySelector('.container')
   })
   
 var button = document.querySelector('button')
 button.addEventListener('click', function(){
-    document.querySelector('.theirMoney').value = 20
+    document.querySelector('.theirMoney').value = parseInt(document.querySelector('.urMoney').value) * rate
 })
