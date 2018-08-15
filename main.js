b33bc91a3f4634bb564c49b6829de4785a8ec7ad
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-08-15 12:56:48 -0600

    got the drop down to work

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-let rate;
+let rateKey;
 
 axios.get('https://api.exchangeratesapi.io/latest')
   .then(function (result) {
-    console.log(result.data.rates['AUD'])
-    rate = result.data.rates['AUD']
+    console.log(result.data.rates)
     document.querySelector('.container')
+    for ( let rateKey in result.data.rates){
+        var option = document.createElement('option')
+        option.value = result.data.rates[rateKey]
+        option.text = rateKey
+        document.querySelector('select').appendChild(option)
+    }
+    var button = document.querySelector('button')
+button.addEventListener('click', function(){
+    document.querySelector('.theirMoney').value = parseInt(document.querySelector('.urMoney').value) * rateKey
+})
   })
   
-var button = document.querySelector('button')
-button.addEventListener('click', function(){
-    document.querySelector('.theirMoney').value = parseInt(document.querySelector('.urMoney').value) * rate
-})
+// var button = document.querySelector('button')
+// button.addEventListener('click', function(){
+//     document.querySelector('.theirMoney').value = parseInt(document.querySelector('.urMoney').value) * rateKey
+// })
+// for(let i = 0; i < currencyType.length; i++){
+// var dropDown = document.createElement('div');
+// document.querySelector('#currencyTypes').appendChild(div);
+// }
+// input.Types.of.Currency = rate[result.data.rates]
