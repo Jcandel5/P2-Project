c27c914f0a75b9d6c0289c3d1465889b300b724c
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-09-18 13:23:29 -0600

    code corrected

--- main.js
+++ main.js
@@ -4,37 +4,26 @@ axios.get('https://api.exchangeratesapi.io/latest')
   .then(function (result) {
     console.log(result.data.rates)
     document.querySelector('.container2')
-    for ( let rateKey in result.data.rates){
-        var option = document.createElement('option')
-        option.value = result.data.rates[rateKey]
-        option.text = rateKey
-        document.querySelector('select').appendChild(option)
+        for ( let rateKey in result.data.rates){
+            var option = document.createElement('option')
+            option.value = result.data.rates[rateKey]
+            option.text = rateKey
+            document.querySelector('select').appendChild(option)
     }
     document.querySelector('.container2')
-    for ( let rateKey in result.data.rates){
-        var option = document.createElement('option')
-        option.value = result.data.rates[rateKey]
-        option.text = rateKey
-        document.querySelector('#selector2').appendChild(option)
+        for ( let rateKey in result.data.rates){
+            var option = document.createElement('option')
+            option.value = result.data.rates[rateKey]
+            option.text = rateKey
+            document.querySelector('#selector2').appendChild(option)
     }
     var button = document.querySelector('button')
-    button.addEventListener('click', function(event){
-        var local = document.querySelector('.urMoney')
-        localStorage.setItem('Your Moneyzzz', local.value)
-        var e = document.querySelector("#selector2");
-        var selectedExchange = e.options[e.selectedIndex].value;
-        document.querySelector('.theirMoney').value = parseFloat(document.querySelector('.urMoney').value) * parseFloat(selectedExchange)
-        // console.log(result.data.rates[selectedExchange])
+        button.addEventListener('click', function(event){
+            var local = document.querySelector('.urMoney')
+            localStorage.setItem('Your Moneyzzz', local.value)
+            var e = document.querySelector("#selector2");
+            var selectedExchange = e.options[e.selectedIndex].value;
+            document.querySelector('.theirMoney').value = parseFloat(document.querySelector('.urMoney').value) * parseFloat(selectedExchange)
     })
   
 })
-  
-// var button = document.querySelector('button')
-// button.addEventListener('click', function(){
-//     document.querySelector('.theirMoney').value = parseInt(document.querySelector('.urMoney').value) * rateKey
-// })
-// for(let i = 0; i < currencyType.length; i++){
-// var dropDown = document.createElement('div');
-// document.querySelector('#currencyTypes').appendChild(div);
-// }
-// input.Types.of.Currency = rate[result.data.rates]
